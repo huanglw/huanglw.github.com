fb023b85d5571877a3d9c6cc4064b75fedbb96e3
huanglw <1289054631@qq.com>
2017-06-24 21:25:17 +0800

    add swal alert window
    add swal alert window

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
-	$("#advertise").show();
+	// $("#advertise").show();
+	// setTimeout("ad_hide()",5000);
+	swal({
+		  title: "Sweet!",
+		  text: "Here's a custom image.",
+		  imageUrl: "images/thumbs-up.jpg"
+		});
 	setTimeout("ad_hide()",5000);
 });
 
 function ad_hide(){
-	$("#advertise").hide();
+	//$("#advertise").hide();
+	swal.close();
 }
