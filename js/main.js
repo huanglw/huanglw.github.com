5cf65679b172baaf922b2408e9b33f0a8f0118e2
huanglw <1289054631@qq.com>
2017-06-27 13:20:25 +0800

    add folder
    add folder examples and file flex_layout.html

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 	// setTimeout("ad_hide()",5000);
 	swal({
 		  title: "Sweet!",
-		  text: "Here's an advertisement.等待15秒",
+		  text: "Here's an advertisement.等待3秒",
 		  imageUrl: "images/thumbs-up.jpg"
 		});
-	setTimeout("ad_hide()",15000);
+	setTimeout("ad_hide()",3000);
 });
 
 function ad_hide(){
