31ea5b9cbbb9da142fa9c4f7551d77b813d38efb
huanglw <1289054631@qq.com>
2017-07-03 17:05:59 +0800

    update products images
    update products images

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
 $(document).ready(function(){
 	// $("#advertise").show();
 	// setTimeout("ad_hide()",5000);
-	swal({
-		  title: "Sweet!",
-		  text: "Here's an advertisement.等待3秒",
-		  imageUrl: "images/thumbs-up.jpg"
-		});
-	setTimeout("ad_hide()",3000);
+	// swal({
+	// 	  title: "Sweet!",
+	// 	  text: "Here's an advertisement.等待3秒",
+	// 	  imageUrl: "images/thumbs-up.jpg"
+	// 	});
+	// setTimeout("ad_hide()",3000);
+	// swal({
+	//   title: "Are you sure?",
+	//   text: "Your will not be able to recover this imaginary file!",
+	//   type: "warning",
+	//   showCancelButton: true,
+	//   confirmButtonColor: "#DD6B55",
+	//   confirmButtonText: "Yes, delete it!",
+	//   closeOnConfirm: false
+	// },
+	// function(){
+	//   swal("Deleted!", "Your imaginary file has been deleted.", "success");
+	// });
+	swal({  
+      title: "Sweet!",  
+      text: "Here's a custom image.",  
+      imageUrl: "images/ad-img.png"  
+	});  
 });
 
-function ad_hide(){
-	//$("#advertise").hide();
-	swal.close();
-}
+// function ad_hide(){
+// 	//$("#advertise").hide();
+// 	swal.close();
+// }
