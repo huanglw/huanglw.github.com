b6bc5a371e67b1f57e0ce43c258e735d2ed0a9f3
huanglw <1289054631@qq.com>
2017-07-11 15:25:44 +0800

    create huanglw.html
    create huanglw.html and stylesheet file

---
+++ examples/js/huanglw.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	getDate();
+	setInterval("getDate()",1000);
+});
+function getDate(){
+	var localDate = new Date();
+	//$("#date").innerHTML = localDate.toLocaleDateString();
+	//document.getElementById("date").innerHTML = localDate.toDateString();
+	document.getElementById("date").innerHTML = localDate.toGMTString();
+}
